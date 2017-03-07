c6a2abc751b08b79362f99c942e91aeba66c4f79
Alex Lee <zhuoyanlee@gmail.com>
2017-03-07 21:42:10 +1100

    Initial commit

---
+++ www/templates/funds/js/controllers.js
@@ -0,0 +1,67 @@
+// Controller of Notes List Page.
+// It will call NoteDB Services to present data to html view.
+appControllers.controller('fundsListCtrl', function ($http, $scope,$stateParams, $timeout, NoteDB, $state) {
+
+    // initialForm is the first activity in the controller. 
+    // It will initial all variable data and let the function works when page load.
+    $scope.initialForm = function () {
+
+		//Get all funds from web service.
+        $http({
+			method : 'GET',
+			url : 'http://dev-project-midas.pantheonsite.io' + "/funds/list",
+			headers: { 'Authorization': 'Basic ' + btoa('alex:b45k3t')},
+			cache : true, // FIXME need to put an expiry to the cache somehow
+		}).then(function(response) {
+			$scope.noteList = response.data;
+		});
+            
+            
+        //$scope.isLoading is the variable that use for check statue of process.
+        $scope.isLoading = true;
+
+        //$scope.isAnimated is the variable that use for receive object data from state params.
+        //For enable/disable row animation.
+        $scope.isAnimated =  $stateParams.isAnimated;
+
+        // $scope.noteList is the variable that store data from NoteDB service.
+        $scope.noteList = [];
+
+        // $scope.filterText is the variable that use for searching.
+        $scope.filterText = "";
+
+        // The function for loading progress.
+        $timeout(function () {
+            if ($scope.isAndroid) {
+                jQuery('#note-list-loading-progress').show();
+            }
+            else {
+                jQuery('#note-list-loading-progress').fadeIn(700);
+            }
+        }, 400);
+        $timeout(function () {
+
+            
+
+            jQuery('#note-list-loading-progress').hide();
+            jQuery('#note-list-content').fadeIn();
+            $scope.isLoading = false;
+        }, 3000);// End loading progress.
+
+    };//End initialForm.
+
+    // navigateTo is for navigate to other page 
+    // by using targetPage to be the destination page 
+    // and sending objectData to the destination page.
+    // Parameter :  
+    // targetPage = destination page.
+    // objectData = object that will sent to destination page.
+    $scope.navigateTo = function (targetPage, objectData) {
+        $state.go(targetPage, {
+            noteDetail: objectData,
+            actionDelete: (objectData == null ? false : true)
+        });
+    };// End navigateTo.
+
+    $scope.initialForm();
+});// End of Notes List Page  Controller.
